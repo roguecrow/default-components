dd2a7ee45738e146619aca67b0333a31655530a6
baburajkumar <baburajkumar2001@gmail.com>
2023-04-27 23:55:46 +0530

    6th

--- lib/home_drawer.dart
+++ lib/home_drawer.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:tutorial2/pages/fifth_page.dart';
 import 'package:tutorial2/pages/first_page.dart';
 import 'package:tutorial2/pages/fourth_page.dart';
 import 'package:tutorial2/pages/second_page.dart';
@@ -13,8 +15,16 @@ class HomeDrawer extends StatefulWidget {
 }
 
 class _HomeDrawerState extends State<HomeDrawer> {
+  var _Count = 0;
+
+  void _incrementNotificationCount() {
+    setState(() {
+      _Count++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    var countString = _Count.toString();
     return Scaffold(
       appBar: AppBar(
         title:const Text('MyApp'),
@@ -73,7 +83,11 @@ class _HomeDrawerState extends State<HomeDrawer> {
           ),
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.notifications),
+            icon: const Badge(
+              label: Text('' ,style: TextStyle(color: Colors.white)),
+              backgroundColor: Colors.red,
+              child: Icon(Icons.notifications),
+            ),
           ),
         ],
 
@@ -122,27 +136,59 @@ class _HomeDrawerState extends State<HomeDrawer> {
                   fontSize: 20,
                 ),
                 ),
-                onTap: (){
+                onTap: () {
                   Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const SecondPage())
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                      const SecondPage(),
+                      transitionDuration: const Duration(milliseconds: 500),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        return PositionedTransition(
+                            rect: RelativeRectTween(
+                              begin: RelativeRect.fromSize(
+                                  const Rect.fromLTWH(0, 0, 0, 0),
+                                const Size(0, 0),
+                              ),
+                              end: RelativeRect.fromSize(
+                                  const Rect.fromLTWH(0, 0, 1, 1),
+                                const Size(1, 1),
+                              ),
+                            ).animate(animation),
+                            child: child,
+                        );
+                      },
+                    ),
                   );
                 },
               ),
               ListTile(
-                leading: const Icon(Icons.list,
+                leading: const Icon(
+                  Icons.list,
                   color: Colors.white,
                 ),
-                title: const Text('BOTTOM NAV BAR',style:TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+                title: const Text(
+                  'BOTTOM NAV BAR',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
-                ),
-                onTap: (){
+                onTap: () {
                   Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const ThirdPage())
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                      const ThirdPage(),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        return FadeTransition(
+                          opacity: animation,
+                          child: child,
+                        );
+                      },
+                    ),
                   );
                 },
               ),
+
               ListTile(
                 leading: const Icon(Icons.list,
                   color: Colors.white,
@@ -152,9 +198,48 @@ class _HomeDrawerState extends State<HomeDrawer> {
                   fontSize: 20,
                 ),
                 ),
-                onTap: (){
+                onTap: () {
                   Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const FourthPage())
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                      const FourthPage(),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        return SlideTransition(
+                            position: Tween<Offset>(
+                              begin: const Offset(1.0, 0.0),
+                              end: Offset.zero,
+                            ).animate(animation),
+                          child: child,
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.list,
+                  color: Colors.white,
+                ),
+                title: const Text('CHECKBOX LISTVIEW',style:TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                      const FifthPage(),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        return RotationTransition(
+                            turns: Tween<double>(
+                              begin: 0.0,
+                              end: 1.0,
+                            ).animate(animation),
+                          child: child,
+                        );
+                      },
+                    ),
                   );
                 },
               ),
@@ -167,6 +252,11 @@ class _HomeDrawerState extends State<HomeDrawer> {
           'hello',
           style: TextStyle(fontSize: 24),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementNotificationCount,
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.add),
       ),
     );
   }
